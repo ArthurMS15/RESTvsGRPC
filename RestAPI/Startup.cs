07e5512e1c3ea590a55dac03bbeda2ae23eb1d11
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-09-03 19:52:01 -0300

    update aspnetcoreapp

--- RestAPI/Startup.cs
+++ RestAPI/Startup.cs
@@ -28,8 +28,26 @@ namespace RestAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
 
-            app.UseMvc();
-        }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }       
+
     }
 }
