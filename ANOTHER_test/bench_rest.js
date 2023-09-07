06628f2fd78469d5f3da302038a354d831e0c905
ArthurMS15 <66339390+ArthurMS15@users.noreply.github.com>
2023-09-06 23:55:21 -0300

    correção benchmark

--- ANOTHER_test/bench_rest.js
+++ ANOTHER_test/bench_rest.js
@@ -1,28 +1,63 @@
 const axios = require('axios');
-const NUM_REQUESTS = 1000;  // You can adjust this
+const NUM_REQUESTS = 1000;
 
 let startTime;
 let completedRequests = 0;
+let requestTimes = [];
+let failedRequests = 0;
 
-// Fetch the list of products from the server first
-axios.get('http://localhost:5000/products')  // Assuming there's an endpoint that lists all products
+axios.get('http://localhost:5000/products')
     .then(response => {
-        const data = response.data;  // Here, data is populated with the list of products from the server
+        const data = response.data;
 
-        startTime = Date.now();  // Start the timer
+        startTime = Date.now();
 
-        // Now, initiate the benchmarking requests
         for (let i = 0; i < NUM_REQUESTS; i++) {
-            axios.get(`http://localhost:5000/product/${i % data.length}`)  // Cycle through data
-                .then(response => {
-                    completedRequests++;
-                    if (completedRequests === NUM_REQUESTS) {
-                        let endTime = Date.now();
-                        console.log(`REST: ${NUM_REQUESTS} requests took ${(endTime - startTime)}ms`);
-                    }
+            const reqStartTime = Date.now();
+            axios.get(`http://localhost:5000/product/${i % data.length}`)
+                .then(() => {
+                    const reqEndTime = Date.now();
+                    const elapsedTime = reqEndTime - reqStartTime;
+                    requestTimes.push(elapsedTime);
                 })
                 .catch(error => {
                     console.error('Error in request:', error);
+                    failedRequests++;
+                })
+                .finally(() => {
+                    completedRequests++;
+                    if (completedRequests === NUM_REQUESTS) {
+                        const endTime = Date.now();
+                        console.log(`REST: ${NUM_REQUESTS} requests took ${(endTime - startTime)}ms`);
+                        console.log(`${failedRequests} requests failed.`);
+
+                        requestTimes.sort((a, b) => a - b); // Sort request times
+
+                        const minTime = requestTimes[0];
+                        const maxTime = requestTimes[requestTimes.length - 1];
+                        const medianTime = requestTimes[Math.floor(requestTimes.length / 2)];
+                        const totalTime = requestTimes.reduce((acc, curr) => acc + curr, 0);
+                        const meanTime = totalTime / requestTimes.length;
+
+                        console.log(`Connection Times (ms)`);
+                        console.log(`min: ${minTime} ms, mean: ${meanTime.toFixed(2)} ms, median: ${medianTime} ms, max: ${maxTime} ms`);
+
+                        // Calculate percentiles
+                        const calculatePercentile = (percentile) => {
+                            return requestTimes[Math.floor(requestTimes.length * percentile)];
+                        };
+
+                        console.log(`Percentage of the requests served within a certain time (ms)`);
+                        console.log(`50%: ${calculatePercentile(0.5)} ms`);
+                        console.log(`66%: ${calculatePercentile(0.66)} ms`);
+                        console.log(`75%: ${calculatePercentile(0.75)} ms`);
+                        console.log(`80%: ${calculatePercentile(0.8)} ms`);
+                        console.log(`90%: ${calculatePercentile(0.9)} ms`);
+                        console.log(`95%: ${calculatePercentile(0.95)} ms`);
+                        console.log(`98%: ${calculatePercentile(0.98)} ms`);
+                        console.log(`99%: ${calculatePercentile(0.99)} ms`);
+                        console.log(`100%: ${maxTime} ms (longest request)`);
+                    }
                 });
         }
     })
--- ANOTHER_test/strbench_grpc.js
+++ ANOTHER_test/strbench_grpc.js
@@ -16,24 +16,53 @@ const ecommerceProto = grpc.loadPackageDefinition(packageDefinition).ecommerce;
 
 const client = new ecommerceProto.EcommerceService('localhost:50052', grpc.credentials.createInsecure());
 
-const NUM_REQUESTS = 1000;  
+const NUM_REQUESTS = 1000;
 
 let startTime = Date.now();
 let completedRequests = 0;
+let responseTimes = []; // To track each request's response time
 
 for (let i = 0; i < NUM_REQUESTS; i++) {
+  const reqStartTime = Date.now();
   const call = client.GetProductStream({ product_id: `${i}` });
-  
+
   call.on('data', (response) => {
     // Handle streamed back response
     console.log(response);
   });
-  
+
   call.on('end', () => {
+    const reqEndTime = Date.now();
+    responseTimes.push(reqEndTime - reqStartTime);
+
     completedRequests++;
     if (completedRequests === NUM_REQUESTS) {
       let endTime = Date.now();
       console.log(`gRPC Streaming: ${NUM_REQUESTS} requests took ${(endTime - startTime)}ms`);
+
+      // Calculate and print the additional metrics
+      responseTimes.sort((a, b) => a - b); // Sort times
+      const min = responseTimes[0];
+      const max = responseTimes[NUM_REQUESTS - 1];
+      const median = responseTimes[Math.floor(NUM_REQUESTS / 2)];
+      const mean = responseTimes.reduce((sum, time) => sum + time, 0) / NUM_REQUESTS;
+      // TODO: You might want to calculate standard deviation as well
+
+      console.log(`Connection Times (ms)
+                     min    mean    median   max
+          Total:     ${min}   ${mean.toFixed(2)}   ${median}    ${max}`);
+
+      console.log(`
+          Percentage of the requests served within a certain time (ms)
+            50%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.50)]}
+            66%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.66)]}
+            75%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.75)]}
+            80%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.80)]}
+            90%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.90)]}
+            95%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.95)]}
+            98%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.98)]}
+            99%   ${responseTimes[Math.floor(NUM_REQUESTS * 0.99)]}
+          100%   ${max} (longest request)`);
     }
   });
 }
